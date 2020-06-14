2a78d13a6c95a5ab0fcff2bffaeaca0da4143cb4
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-06-13 22:29:07 -0500

    Create massWrapper.sh

---
+++ test/massWrapper.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $3 == "NULL" ]
+then
+    exit -1
+fi
+
+dir=$1
+cms=$2
+sarch=$3
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export SCRAM_ARCH=$sarch
+scramv1 project CMSSW $cms
+cd $cms
+cd src
+cmsenv
+
+cd ../..
+cp -r $dir/$cms/. $cms/.
+#cmsRun PSetDump.py
+
+cp PSetDump.py pset.py
+python psetEditWrapper.py foo
+
+method=$(/usr/libexec/condor/condor_chirp get_job_attr ChirpTransferMethod)
+if (method=="cms")
+then
+    trap 'exit' ERR
+    cmsRun psetB.py
+fi
+cmsRun psetA.py
