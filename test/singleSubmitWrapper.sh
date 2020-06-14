b14f5b9e06fef6860e0f15ab4454ecdd760936b0
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-06-13 23:24:09 -0500

    Update singleSubmitWrapper.sh

--- test/singleSubmitWrapper.sh
+++ test/singleSubmitWrapper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-index = $1
+index=$1
 if [ $index == "NULL" ]
 then
-    exit index = 1
+    exit
 fi
 
 for i in {1..100}
@@ -12,7 +12,7 @@ do
     cms=$(sed -n "${index}p" /home/bloomcap3/listcms.txt)
     sarch=$(sed -n "${index}p" /home/bloomcap3/listarch.txt)
 
-    echo "The variables: ${dir} ${cms} ${arch}"
+    echo "The variables: ${dir} ${cms} ${sarch} ${i}"
 
     condor_submit singleWrapper.sub -append "Arguments = ${dir} ${cms} ${sarch} ${i}" "transfer_input_files = /home/bloomcap3/$dir, /home/bloomcap3/$dir/PSetDump.py, /home/bloom/yanfr0818/IDDS/condor/psetB.py, /home/bloom/yanfr0818/IDDS/condor/psetEditWrapper.py" "transfer_output_files = jobreport$i.xml, jobreportA$i.xml, jobreportB$i.xml" "Error = singleWrapper/err.$i" "Output = singleWrapper/out.$i" "Log = singleWrapper/log.$i" 
 
