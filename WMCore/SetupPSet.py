35a0627e3c0f156bdf1f74e17ef36e448bed60df
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-08-13 18:54:12 -0500

    Update SetupPSet.py

--- WMCore/SetupPSet.py
+++ WMCore/SetupPSet.py
@@ -52,47 +52,57 @@ class CMSSWPset():
             print('output: ',getattr(self.process,outMod).fileName)
         return
 
+    def chirpSetAttr(attr, val):
+        condor_chirp = '/usr/libexec/condor/condor_chirp'
+        process = subprocess.Popen([condor_chirp, 'set_job_attr', attr,'\''+val+'\''], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = process.communicate()
         
 def main():
         
-        shutil.copyfile( 'pset.py', 'psetA.py')
+    shutil.copyfile( 'pset.py', 'psetA.py')
         
-        pset_job = CMSSWPset('pset')
-        psetA    = CMSSWPset('psetA')
-        psetB    = CMSSWPset('psetB')
+    pset_job = CMSSWPset('PSetDump')
+    psetA    = CMSSWPset('psetA')
+    psetB    = CMSSWPset('psetB')
 
-        # Check process.source exists
-        if getattr(pset_job.process, "source", None) is None:
-            msg = "Error in CMSSW PSet: process is missing attribute 'source'"
-            msg += " or process.source is defined with None value."
-            raise RuntimeError(msg)
+    # Check process.source exists
+    if getattr(pset_job.process, "source", None) is None:
+        msg = "Error in CMSSW PSet: process is missing attribute 'source'"
+        msg += " or process.source is defined with None value."
+        raise RuntimeError(msg)
         
-        inputFiles = pset_job.process.source.fileNames
-        for outMod in psetB.process.outputModules.keys():
-            outputFiles = getattr(psetB.process,outMod).fileName
-            break
+    inputFiles = pset_job.process.source.fileNames
+    for outMod in psetB.process.outputModules.keys():
+        outputFiles = getattr(psetB.process,outMod).fileName
+        break
             
+    method = random.randint(0,1)
+    if (method == 0):
+        transfer = 'cms'
         psetB.swap(pset_job, 'input')
         psetA.swap(psetB, 'outputToInput')
         psetA.swap(pset_job, 'nEvent')
-        psetB.swap(pset_job, 'nEvent')
+        psetB.swap(pset_job, 'nEvent')  
+    else:
+        transfer = 'default'
+    chirpSetAttr('ChirpTransferMethod', transfer)
+    
+    workingDir   = os.getcwd()
+    configPickle = ['ppsetA.pkl', 'ppsetB.pkl']
+    configFile   = [x.replace('.pkl','.py') for x in configPickle]
         
-        workingDir   = os.getcwd()
-        configPickle = ['ppsetA.pkl', 'ppsetB.pkl']
-        configFile   = [x.replace('.pkl','.py') for x in configPickle]
-        
-        for i in range(2):
-          try:
-             with open("%s/%s" % (workingDir, configPickle[i]), 'wb') as pHandle:
-                pickle.dump(psetB.process, pHandle)
-
-             with open("%s/%s" % (workingDir, configFile[i]), 'w') as handle:
-                handle.write("import FWCore.ParameterSet.Config as cms\n")
-                handle.write("import pickle\n")
-                handle.write("with open('%s', 'rb') as handle:\n" % configPickle[i])
-                handle.write("    process = pickle.load(handle)\n")
-          except Exception as ex:
-            raise ex
+    for i in range(2):
+      try:
+         with open("%s/%s" % (workingDir, configPickle[i]), 'wb') as pHandle:
+            if configPickle[i] == 'ppsetA.pkl': pickle.dump(psetA.process, pHandle)
+            if configPickle[i] == 'ppsetB.pkl': pickle.dump(psetB.process, pHandle)
+         with open("%s/%s" % (workingDir, configFile[i]), 'w') as handle:
+            handle.write("import FWCore.ParameterSet.Config as cms\n")
+            handle.write("import pickle\n")
+            handle.write("with open('%s', 'rb') as handle:\n" % configPickle[i])
+            handle.write("    process = pickle.load(handle)\n")
+      except Exception as ex:
+        raise ex
    
 if __name__ == "__main__":
     main()
