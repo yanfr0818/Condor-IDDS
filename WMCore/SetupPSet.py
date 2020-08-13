2107c1f2cbbf0c9906dc85e3ff4aef22ac42aa1b
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-08-13 04:48:21 -0500

    Update SetupPSet.py

--- WMCore/SetupPSet.py
+++ WMCore/SetupPSet.py
@@ -31,12 +31,12 @@ class CMSSWPset():
     def swap(self, process='input', fName=''):
         
         if process == 'input':
-            fName = str(fName)
+            #fName = str(fName)
             try:    fName = fName.replace('.string' ,'.vstring')    
             except: pass
             print("file name: ",fName)
-            if fName.find('/') == -1:    fName = fName.replace("string(\'", "string(\'file:")
-            setattr(self.process, str(self.process.source.fileNames), str(fName))
+            #if fName.find('/') == -1:    fName = fName.replace("string(\'", "string(\'file:")
+            setattr(self.process, self.process.source.fileNames, fName)
             
         if process == 'output':
             try:    fName = fName.replace('vstring','string' )    
