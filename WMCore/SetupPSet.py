2382444c2d1035f211255645cd5325d026c91acd
yanfr0818 <40543999+yanfr0818@users.noreply.github.com>
2020-08-12 22:33:02 -0500

    Update SetupPSet.py

--- WMCore/SetupPSet.py
+++ WMCore/SetupPSet.py
@@ -255,7 +255,7 @@ def main():
 
         psetTweak = "pset.py"
         if psetTweak is not None:
-            mySetup.applyTweak(psetTweak, mySetup.fixupDict)
+            mySetup.applyTweak(psetTweak)
         
         try:
             with open("pset_new.py", 'wb+') as pHandle:
